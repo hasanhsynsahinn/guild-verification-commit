2eaf70ab8a6de1ad6c5ac886cc20931160111b21
hasan <130412426+hasanhsynsahinn@users.noreply.github.com>
2026-03-05 15:25:23 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:24:45.770Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T12:25:28.418Z
+Commit: Correct typo in configuration file
