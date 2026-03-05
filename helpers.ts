4e4701a21e81f36a4ec2a1abcd551cf460c2b859
hasan <130412426+hasanhsynsahinn@users.noreply.github.com>
2026-03-05 15:02:16 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:02:16.435Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T12:02:21.892Z
+Commit: Fix CSS styling bug on mobile devices
